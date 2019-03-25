e9349b33aab4c8c9ec3d194b07daaea5998bde0f
ElizaHlglh <ewu4@stuy.edu>
2019-03-25 02:46:53 -0400

    MyDeque.java

--- MyDeque.java
+++ MyDeque.java
@@ -10,6 +10,7 @@ public class MyDeque<E>{
     start = -1;
     end = -1;
   }
+  
   public MyDeque(int initialCapacity){
     if (initialCapacity < 0){
       throw new IllegalArgumentException("array length < 0");
@@ -19,6 +20,7 @@ public class MyDeque<E>{
     start = -1;
     end = -1;
   }
+
   public int size(){
     int ans = 0;
     for (int i = 0; i < data.length; i++){
@@ -28,6 +30,7 @@ public class MyDeque<E>{
     }
     return ans;
   }
+
   public String toString(){
     String ans = "{";
     for (int i = 0; i < data.length; i++){
@@ -41,38 +44,107 @@ public class MyDeque<E>{
     ans += "}";
     return ans;
   }
-  public void addFirst(E element){
-    if (size == data.length){
-      resize();
-    }
-    //shift the array one side over
-    if (start != -1){//if the array isn't empty
 
+  public void addFirst(E element){
+    if (start != -1){//if the array isn't empty
+      if (size == data.length){
+        resize();
+      }
+      if (start == 0){//if current start is at 0, add the new element/start to the back of the array
+        data[data.length-1] = element;
+        start = data.length-1;
+        size++;
+      }
+      else{
+        data[start-1] = element;
+        start--;
+        size++;
+      }
+    }
+    else{//if the array is empty
+      start = 0;
+      end = 0;
+      resize();
+      data[0] = element;
+      size++;
     }
   }
-  public void addLast(E element){ }
+
+  public void addLast(E element){
+    if (end != -1){
+      if (size == data.length){//if full
+        resize();
+      }
+      data[end+1] = element;
+      end++;
+      size++;
+    }
+    else{//if array is empty
+      start = 0;
+      end = 0;
+      resize();
+      data[0] = element;
+      size++;
+    }
+  }
+
   public E removeFirst(){
+    if (size == 0){
+      throw new NoSuchElementException("Can't remove from empty deque!!!");
+    }
     E temp = data[start];
     data[start] = null;
     start++;
+    size--;
     return temp;
   }
+
   public E removeLast(){
+    if (size == 0){
+      throw new NoSuchElementException("Can't remove from empty deque!!!");
+    }
     E temp = data[end];
     data[end] = null;
     end--;
+    size--;
     return temp;
   }
+
   public E getFirst(){
+    if (size == 0){
+      throw new NoSuchElementException("Can't get anything from empty deque!!!");
+    }
     return data[start];
   }
+
   public E getLast(){
+    if (size == 0){
+      throw new NoSuchElementException("Can't get anything from empty deque!!!");
+    }
     return data[end];
   }
+
   public void resize(){
     E[] copy = (E[])new Object[data.length*2 + 1];
-    for (int i = 0; i < data.length; i++){
-      copy[i] = data[i];
+    if (start == 0){//if there is no need for the array to put the part after start into the back of new array
+      for (int i = 0; i < data.length; i++){
+        copy[i] = data[i];
+      }
+    }
+    else{
+      int OldStart = start;
+      int NewStart = copy.length - (data.length - start);
+      start = NewStart;
+      while (OldStart != end){
+        if (OldStart == data.length){
+          OldStart = 0;
+        }
+        copy[NewStart] = data[OldStart];
+        NewStart++;
+        OldStart++;
+      }
+      //after the while loop, the OldStart should be == to end and I need to add that end value to the copy array
+      copy[NewStart] = data[OldStart];
     }
     data = copy;
   }
